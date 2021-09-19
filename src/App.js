4fdb16c1b24c4f92ae1a9001e18b3267212eda9a
showkhin1997 <showkhin15@gmail.com>
2021-09-19 16:48:20 +0600

    react api todo task complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadTodo></LoadTodo>
     </div>
+  );
+}
+
+function LoadTodo() {
+  const [todos, setTodo] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => setTodo(data))
+  }, [])
+
+  return (
+    <div className="todoStyle">
+      {
+        todos.map(todo => <Todo id={todo.id} title={todo.title} completed={todo.completed}></Todo>)
+      }
+    </div>
+  );
+}
+
+function Todo(props) {
+  console.log(props.completed)
+  return (
+    < div className="todo" >
+      <h2>Id: {props.id}</h2>
+      <h3>{props.title}</h3>
+      <h5>Completed: {props.completed.toString()}</h5>
+    </div >
   );
 }
 
